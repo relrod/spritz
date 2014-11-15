4eec3c21fcb64c0f33fca8ed037566f1f118f531
Ricky Elrod <ricky@elrod.me>
2014-11-15 04:38:43 -0500

    Started writing documentation based on the paper.
    Still have a bunch of functions to document, though.
    
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- src/Crypto/Cipher/Spritz.hs
+++ src/Crypto/Cipher/Spritz.hs
@@ -50,13 +50,13 @@ module Crypto.Cipher.Spritz (
   swap,
 
   -- * Making use of everything
-  -- ** Hashing
-  hash,
-
   -- ** Encyrption
   encrypt,
   decrypt,
   keySetup,
+
+  -- ** Hashing
+  hash,
 
   -- ** Message Authentication Code (MAC)
   mac,
@@ -69,31 +69,59 @@ import Control.Monad
 import Data.Bits
 import qualified Data.Vector as V
 
+-- | Register values and 's'. As a __difference to the paper__, we also include
+-- 'n' in the state, for easy access to it within the various functions.
+--
+-- See §3.1 "State".
 data SpritzState = SpritzState
                    { _i :: Int
+                   -- ^ Register: Always incremented by @'w' `'mod'` 'n'@
+                   -- whenever changed.
                    , _j :: Int
+                   -- ^ Register
                    , _k :: Int
+                   -- ^ Register
                    , _z :: Int
+                   -- ^ Register: Last value produced by 'output'
                    , _w :: Int
+                   -- ^ Register: Modified when 'whip' is called. Always
+                   -- relatively prime to 'n'.
                    , _a :: Int
+                   -- ^ Register: Number of nibbles absorbed since start of last
+                   -- 'shuffle' of 's'.
                    , _s :: V.Vector Int
-                   , _n :: Int
+                   -- ^ Length 'n'. Permutation of Z_n.
+                   , _n :: Int          -- ^ All values in Spritz are modulo 'n'.
                    } deriving (Show, Eq, Ord)
 
 makeLenses ''SpritzState
 
-initializeState :: Int -> SpritzState
+-- | Returns the standard initial state. See §3.2 "InitializeState".
+initializeState :: Int         -- ^ The 'n' value to use throughout.
+                -> SpritzState -- ^ The initial state.
 initializeState n' = SpritzState 0 0 0 0 1 0 (V.fromList [0..n'-1]) n'
 
+-- | Takes a variable-lenght input and updates the state based on it. Spritz
+-- absorbs input in blocks of @'floor' ('n' / 2)@ nibbles each (low-order
+-- nibble of each byte first). After each block is absorbed, we call 'shuffle'.
+--
+-- Satisfies the following law:
+--
+--    @'absorb' x1 >> 'absorb' x2 = 'absorb (x1 ++ x2)'@
+--
+-- See §3.2 "Absorb".
 absorb :: V.Vector Int -> State SpritzState ()
 absorb i' =
   mapM_ (\v -> absorbByte $ i' V.! v) [0..V.length i' - 1]
 
+-- | Splits the given input byte into two nibbles and updates state based on
+-- each nibble, low-order nibble first. See §3.2 "AbsorbByte".
 absorbByte :: Int -> State SpritzState ()
 absorbByte b = do
   absorbNibble $ low b
   absorbNibble $ high b
 
+-- | TODO: Write documentation. See §3.2 "AbsorbNibble".
 absorbNibble :: Int -> State SpritzState ()
 absorbNibble x = do
   a' <- use a
@@ -105,6 +133,9 @@ absorbNibble x = do
   swap a'' (plusmod floored x n')
   a .= plusmod a'' 1 n'
 
+-- | Equivalent to absorbing a special "stop" symbol outside of the oridnary
+-- input alphabet. The intent is to provide a clean way to separate different
+-- inputs being absorbed. See §2.1.
 absorbStop :: State SpritzState ()
 absorbStop = do
   a' <- use a
@@ -114,6 +145,10 @@ absorbStop = do
   a'' <- use a
   a .= plusmod a'' 1 n'
 
+-- | 'whip's, 'crush'es, 'whip's, 'crush'es, and finally 'whip's again.
+-- According to the paper, each 'whip' randomizes the state. Calling 'crush'
+-- between each 'whip' causes the effects of 'crush' to be not easily determined
+-- by manipulating the input.  See §3.2 "Shuffle".
 shuffle :: State SpritzState ()
 shuffle = do
   n' <- use n
@@ -124,7 +159,10 @@ shuffle = do
   whip (2 * n')
   a .= 0
 
-whip :: Int -> State SpritzState ()
+-- | Calls 'update' @r@ times. Also updates 'w' to the next largest value that
+-- is relatively prime to 'n'.
+whip :: Int -- ^ @r@. The number of times to call 'update'.
+     -> State SpritzState ()
 whip r = do
   replicateM_ r update
   bumpW
@@ -195,9 +233,16 @@ low b = b .&. 0xf
 high :: (Bits a, Num a,Show a) => a -> a
 high b = b `shiftR` 4 .&. 0xf
 
+-- | A utility function that adds the first parameter to the second then returns
+-- that modulo the third parameter ('n'). This is used throughout Spritz in
+-- place of a more traditional 'xor' approach so that 'n' can be any value and
+-- is not limited to being a power of 2.
 plusmod :: Integral a => a -> a -> a -> a
 plusmod a' b' n' = (a' + b') `mod` n'
 
+
+-- | See 'plusmod'. This is very similar except it subtracts the first two
+-- arguments instead of adding them.
 submod :: Integral a => a -> a -> a -> a
 submod a' b' n' = plusmod n' (a' - b') n'
 
@@ -210,17 +255,11 @@ swap i' j' = do
   s . ix j' .= tmpI
   s . ix i' .= tmpJ
 
--- | Produces an r-byte hash of the input message.
-hash :: V.Vector Int -> Int -> SpritzState -> V.Vector Int
-hash m r = evalState $ do
-  absorb m
-  absorbStop
-  absorb (V.fromList [r .&. 0xff])
-  squeeze r
-
 -----------------------------------------------------------------------------
 -- Encryption
 -----------------------------------------------------------------------------
+-- | Adds-modulo-N ('plusmod') each byte of the message with the corresponding
+-- byte of the output of 'squeeze' yielding an ecrypted ciphertext. See §2.2.
 encrypt :: V.Vector Int -- ^ The key.
         -> V.Vector Int -- ^ The decrypted message.
         -> SpritzState  -- ^ Starting state.
@@ -232,6 +271,8 @@ encrypt k' m' = evalState $ do
   m'' <- squeeze (V.length m')
   return $ V.map (uncurry (\x y -> plusmod x y n')) (V.zip m' m'')
 
+-- | Decrypts a message encrypted with 'encrypt'. Identical to 'encrypt' except
+-- uses 'submod' instead. See §2.2.
 decrypt :: V.Vector Int -- ^ The key.
         -> V.Vector Int -- ^ The encrypted message.
         -> SpritzState  -- ^ Starting state.
@@ -254,8 +295,30 @@ keySetup n' k' =
   put (initializeState n') >> absorb k'
 
 -----------------------------------------------------------------------------
+-- Hashing
+-----------------------------------------------------------------------------
+-- | Produces an @r@-byte hash of the input message.
+--
+-- 'hash' absorbs the input message, calls 'absorbStop' to signal the end of the
+-- input message, then absorbs the desired hash length (@r@).
+--
+-- The given @r@ is absorbed for functional separation.
+--
+-- See § 2.3.
+hash :: V.Vector Int -- ^ The message.
+     -> Int          -- ^ r (number of bytes).
+     -> SpritzState  -- ^ Initial state.
+     -> V.Vector Int
+hash m r = evalState $ do
+  absorb m
+  absorbStop
+  absorb (V.fromList [r .&. 0xff])
+  squeeze r
+
+-----------------------------------------------------------------------------
 -- Message Authentication Code (MAC)
 -----------------------------------------------------------------------------
+-- | Message authentication code. See §2.4.
 mac :: V.Vector Int -- ^ The key.
     -> V.Vector Int -- ^ The message.
     -> Int -- ^ r
